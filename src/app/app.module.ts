495274def9238b9a7d8a0b7165003ddb052822bf
Evgenygroup <mailfrom61@gmail.com>
2020-04-04 10:50:42 +0200

    shipment form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddShipmentComponent } from './components/add-shipment/add-shipment.component';
+import { ValidationErrorsComponent } from './components/validation-errors/validation-errors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddShipmentComponent,
+    ValidationErrorsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-shipment/add-shipment.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {PrintService} from "../../service/print.service";
+
+@Component({
+  selector: 'app-add-shipment',
+  templateUrl: './add-shipment.component.html',
+  styleUrls: ['./add-shipment.component.scss']
+})
+export class AddShipmentComponent implements OnInit {
+  myReactiveForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder,
+              private printService: PrintService) {
+  }
+
+  ngOnInit(): void {
+
+    this.initForm();
+  }
+
+  initForm() {
+    this.myReactiveForm = this.fb.group({
+      description: ['', [Validators.required]],
+      customerId: ['', [Validators.required]]
+    });
+  }
+
+  getControl(controlName: string): AbstractControl {
+    return this.myReactiveForm.get(controlName);
+  }
+
+  onSubmit() {
+    const controls = this.myReactiveForm.controls;
+
+    if (this.myReactiveForm.invalid) {
+      Object.keys(controls).forEach(controlName => controls[controlName].markAsTouched());
+      return;
+    }
+    this.printService.print(this.myReactiveForm.value);
+  }
+}
