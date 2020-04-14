ca619f8b50931b6a9648b92837e7026a528559dc
Alex Loginov <alexander.loginov@takeaway.com>
2020-04-14 20:22:50 +0200

    customer-shipment.component.ts - added solution how to flatten "Observable in observable" with switchMap

--- src/app/components/customer-layout/customer-shipment/customer-shipment.component.ts
+++ src/app/components/customer-layout/customer-shipment/customer-shipment.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, ParamMap} from '@angular/router';
 import {CustomerService} from "../../../service/customer-service/customer.service";
 import {Shipment} from "../../../model/shipment/shipment";
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-customer-shipment',
@@ -16,12 +17,21 @@ export class CustomerShipmentComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
+    // Observable in Observable - valid solution but can be done in other way - pls see below
+    /*this.route.paramMap.subscribe(params => {
       this.customerService.getCustomerShipments(Number(params.get('customerId'))).subscribe(c => {
         this.shipments = c;
         this.customerId = params.get('customerId');
       })
-    });
+    });*/
+
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+          this.customerId = params.get('customerId');
+          return this.customerService.getCustomerShipments(Number(this.customerId))
+        }
+      )
+    ).subscribe( (shipments: Shipment[]) => this.shipments = shipments);
 
   }
 }
